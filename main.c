da88ea59ae5b6d696b51344e012bea60dfc07fcf
gaeul910 <chw910@chungbuk.ac.kr>
2022-10-12 22:02:02 +0900

    new file:   main.c \

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#define MAXDATA 256
+
+typedef struct Stack
+{
+    int data[256];
+    int topindex;
+} Stack;
+
+Stack *init()
+{
+    Stack *stack = (Stack *)malloc(sizeof(Stack));
+    return stack;
+}
+
+int stackPush(int newdata, Stack *stack)
+{
+    if (stack->topindex < MAXDATA)
+    {
+        stack->data[++stack->topindex] = newdata;
+        return 0;
+    }
+}
+
+int main()
+{
+    printf(">>> ");
+    char input[MAXDATA];
+    gets(input);
+    int temp = 1;
+
+    Stack *stack = init();
+
+    for (int i = 0; input[i] != 10; i++) // input[i]가 \0이 아닐 동안
+    {
+        if (input[i] >= '0' && input[i] <= '9')
+        {
+            for (int j = 1; input[i] != ' '; j *= 10)
+            {
+                temp += input[i] - '0';
+                temp *= j;
+            }
+            stackPush(temp, stack);
+        }
+    }
+}
