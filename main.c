a78db5008b90cf20de8bb3997a2faaaee092fa91
1Parksanghyun <107338528+1Parksanghyun@users.noreply.github.com>
2022-10-14 19:58:51 +0900

    postfix 계산추가
    계산기능 추가.
    pop 함수는 int를 리턴하도록 선언 바람.

--- main.c
+++ main.c
@@ -7,6 +7,19 @@ typedef struct Stack
     int topindex;
 } Stack;
 
+int check_operator(char input) {
+    switch (input){
+    case '+':
+        return 1;
+    case '-':
+        return 2;
+    case '/':
+        return 3;
+    case '*':
+        return 4;
+    }
+}
+
 int push(double data, Stack *stack)
 {
     if (stack->topindex <= MAXDATA - 1)
@@ -48,6 +61,30 @@ int main()
                 temp *= j;
             }
             push(temp, stack);
+        } else if (int check = check_operator(input[i]) {
+            switch (check)
+            {
+            case 1:
+                num1 = pop; //pop은 int를 반환하도록 선언해주길 바람.
+                num2 = pop;
+                push(num2+num1, stack);
+                continue;
+            case 2:
+                num1 = pop;
+                num2 = pop;
+                push(num2 - num1, stack);
+                continue;
+            case 3:
+                num1 = pop;
+                num2 = pop;
+                push(num2 / num1, stack);
+                continue;
+            case 4:
+                num1 = pop;
+                num2 = pop;
+                push(num2 * num1, stack);
+                continue;
+            }
         }
     }
 }
